a32199abf9595636d5be2303aefe02870b968c14
diego-lopez8 <diego50421@gmail.com>
2021-10-28 18:00:27 -0400

    initial commit

--- etl_code/CleanMapper.java
+++ etl_code/CleanMapper.java
@@ -9,21 +9,21 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
-
+import org.apache.hadoop.io.NullWritable;
 public class CleanMapper
-    extends Mapper<LongWritable, Text, Text, IntWritable> {
+    extends Mapper<LongWritable, Text, Text, NullWritable> {
     private static final String DELIMETER = ",";
     
     @Override
     public void map(LongWritable key, Text value, Context context)
             throws IOException, InterruptedException {
-        line = value.toString();
-        String artistID;
-        String artistName;
-        String albumID;
-        String trackName;
-        String trackID;
-        String albumName;
+        String line = value.toString();
+        String artistID     = "";
+        String artistName   = "";
+        String albumID      = "";
+        String trackName    = "";
+        String trackID      = "";
+        String albumName    = "";
         boolean albumRefsFound  = false;
         boolean albumIdFound    = false;
         boolean artistNameFound = false;
@@ -46,6 +46,12 @@ public class CleanMapper
                 i++;
                 int j = i;
                 while (!(line.substring(i, i+1).equals(","))) {
+                    /*
+                    Note: this wont work if there is a "," in the actual field itself, like the album name
+                    I could instead use the ending ' as the ending character to searcch for, but this just transfers the problem
+                    because the field can also have a ' in the field name as well. So I'm choosing to stick with failing the comma case
+                    but either case is equally likely to cause a failure of the algorithm
+                    */
                     i++;
                 }
                 albumID = line.substring(j,i);
@@ -119,8 +125,15 @@ public class CleanMapper
                 }
             }
             i++;
-        } 
-        String newLine = String.join(",", artistName, artistID, albumName, albumID, trackName, trackID);
-        context.write(new Text(newLine), new NullWritable());
+        }
+	if (trackID.length() == 22 && albumID.length() == 22 && artistID.length() == 22) {  
+        /*
+        Spotify IDs are all 22 in length. If for some reason they are not length() == 22, then something messed up and we dont write the column
+        If the trackID is not valid, then we cannot perform a join with the features dataset because the key is the trackID
+        */
+		String newLine = String.join(",", artistName, artistID, albumName, albumID, trackName, trackID);
+      		context.write(new Text(newLine), NullWritable.get());
+	}
     }
+
 }
