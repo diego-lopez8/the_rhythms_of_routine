1a85f15a74b703c9c124e837331e5ad6b331988e
diego-lopez8 <diego50421@gmail.com>
2021-11-14 17:44:31 -0500

    added comment for depreciated format

--- ana_code/valence_avg.sql
+++ ana_code/valence_avg.sql
@@ -1,3 +1,5 @@
+-- this file is the depreciated version of valence_union_avg.sql as it runs extremely slow...
+use dtl310;
 SELECT 
     avg(full_traffic_jam.valence) AS Traffic_jam_valence, 
     avg(full_party.valence) AS Party_valence,
