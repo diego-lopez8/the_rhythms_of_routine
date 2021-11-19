d07b77c2c4fb017bde9fde00c3c0cb79ba50a0ac
diego-lopez8 <diego50421@gmail.com>
2021-11-19 15:26:49 -0500

    added KS test

---
+++ ana_code/ks_analysis.py
@@ -0,0 +1,23 @@
+"""
+File: ks_analysis.py
+Author: Diego Lopez
+Date: 11/19/2021
+This file takes the tables loaded from the code in etl/code/ .hql files and performs a KS 2 sample KS test 
+
+"""
+import sys
+sys.path.append('KS-2Samp-PySparkSQL/')
+from ks_2samp_sparksql import *
+from pyspark import SparkContext, SparkConf
+from pyspark.conf import SparkConf
+from pyspark.sql import SparkSession, HiveContext
+
+sparkSession = (SparkSession
+                .builder
+                .appName('example-pyspark-read-and-write-from-hive')
+                .config("hive.metastore.uris", "thrift://localhost:9083", conf=SparkConf())
+                .enableHiveSupport()
+                .getOrCreate()
+                )
+
+sparkSession.sql('use dtl310')
