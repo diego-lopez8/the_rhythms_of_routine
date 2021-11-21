6d968cc61384b9a8055fcc8fd10edc806c32f003
diego-lopez8 <diego50421@gmail.com>
2021-11-21 15:41:43 -0500

    added album and artist frequency

--- ana_code/highest_frequency/sunday_counts.sql
+++ ana_code/highest_frequency/sunday_counts.sql
@@ -14,3 +14,36 @@ ORDER BY
   `value_occurrence` DESC
 
 LIMIT 10;
+
+use dtl310;
+SELECT
+  artistname,
+  COUNT(artistname) AS `value_occurrence` 
+
+FROM
+  full_sunday
+
+GROUP BY 
+  artistname
+
+ORDER BY 
+  `value_occurrence` DESC
+
+LIMIT 10;
+
+use dtl310;
+SELECT
+  albumname, 
+  artistname,
+  COUNT(albumname) AS `value_occurrence` 
+
+FROM
+  full_sunday
+
+GROUP BY 
+  albumname, artistname
+
+ORDER BY 
+  `value_occurrence` DESC
+
+LIMIT 10;
